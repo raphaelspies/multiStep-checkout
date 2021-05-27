176fbe281ac19a4abcebe35adf90daf95ee3c889
raphspies <raphspies@gmail.com>
2021-05-26 22:06:47 -0700

    basic setup: package.json, .gitignore, file structure with babel

---
+++ client/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var App = () => {
+
+  return (
+    <div>
+      Hello from React!
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
