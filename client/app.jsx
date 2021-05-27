77f4c85726e7c56bdc9c97280f2734cf2bad7c4d
raphspies <raphspies@gmail.com>
2021-05-27 11:42:48 -0700

    Added forms 1-3 with input fields and submit buttons, plus conditional rendering in App

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,9 @@
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super(props)
     this.state = {
-      whichScreen: "CreateAccount"
+      whichScreen: "EnterPaymentInfo"
     }
   }
 
@@ -14,10 +14,22 @@ class App extends React.Component {
           <CreateAcount />
         </div>
       )
+    } else if (this.state.whichScreen === "EnterShippingInfo") {
+      return (
+        <div>
+          <EnterShippingInfo />
+        </div>
+      )
+    } else if (this.state.whichScreen === "EnterPaymentInfo") {
+      return (
+        <div>
+          <EnterPaymentInfo />
+        </div>
+      )
     }
   }
 }
-
+//Form1
 var CreateAcount = () => {
   var handleSubmit = (event) => {
     console.log('create account clicked!')
@@ -26,11 +38,73 @@ var CreateAcount = () => {
   return (
     <div id="createAccount">
       <form>
-        Creat Account: <br></br>
-        Name: &nbsp; &nbsp; &nbsp; &nbsp;<input id="createName" placeholder="Enter name"></input> <br></br>
-        Email: &nbsp; &nbsp;&nbsp; &nbsp; <input id="createEmail" placeholder="Enter email addres"></input> <br></br>
-        Password:  <input id="createPassword" placeholder="Enter password"></input> <br></br>
+        <h2> Create Account: </h2>
+        <label htmlFor="createName" className="label">Name:</label>
+        <input id="createName" className ="inputField" placeholder="Enter name"></input> <br></br>
+        <label htmlFor="createEmail" className="label">Email: </label>
+        <input id="createEmail" className ="inputField" placeholder="Enter email address"></input> <br></br>
+        <label htmlFor="createEmail" className="label">Password: </label>
+        <input id="createPassword" className ="inputField" placeholder="Enter password"></input> <br></br><br></br>
         <button id="createAccountButton" onClick={handleSubmit}>Submit</button> <br></br>
+      </form>
+    </div>
+  )
+}
+
+//Form2
+var EnterShippingInfo = () => {
+  var handleSubmit = (event) => {
+    console.log('shipping info clicked!')
+    event.preventDefault()
+  }
+  return (
+    <div id="enterShippingInfo">
+      <form>
+        <h2> Enter Shipping Information: </h2>
+        <label htmlFor="address1" className="label">Address (line 1):</label>
+        <input id="address1" className ="inputField" placeholder="Enter Address, line 1"></input> <br></br>
+
+        <label htmlFor="address2" className="label">Address (line 2): </label>
+        <input id="address2" className ="inputField" placeholder="Enter Address, line 2"></input> <br></br>
+
+        <label htmlFor="enterCity" className="label">City: </label>
+        <input id="enterCity" className ="inputField" placeholder="Enter city"></input> <br></br>
+
+        <label htmlFor="enterState" className="label">State: </label>
+        <input id="enterState" className ="inputField" placeholder="Enter state"></input> <br></br>
+
+        <label htmlFor="enterZip" className="label">Zip Code: </label>
+        <input id="enterZip" className ="inputField" placeholder="Enter zip code"></input> <br></br> <br></br>
+
+        <button id="createAccountButton" onClick={handleSubmit}>Submit</button> <br></br>
+      </form>
+    </div>
+  )
+}
+
+//Form3
+var EnterPaymentInfo = () => {
+  var handleSubmit = (event) => {
+    console.log('Submit Payment clicked!')
+    event.preventDefault()
+  }
+  return (
+    <div id="enterPaymentInfo">
+      <form>
+        <h2> Enter Payment Info: </h2>
+        <label htmlFor="ccNo" className="label">Enter CC Number:</label>
+        <input id="ccNo" className ="inputField" placeholder="Enter Credit Card Number"></input> <br></br>
+
+        <label htmlFor="ccExpiry" className="label">Enter Expiration: </label>
+        <input id="ccExpiry" className ="inputField" placeholder="MM/YYYY"></input> <br></br>
+
+        <label htmlFor="enterCVV" className="label">CVV: </label>
+        <input id="enterCVV" className ="inputField" placeholder="CVV"></input> <br></br>
+
+        <label htmlFor="billingZip" className="label">Zip Code: </label>
+        <input id="billingZip" className ="inputField" placeholder="Enter zip code"></input> <br></br> <br></br>
+
+        <button id="submitPaymentButton" onClick={handleSubmit}>Submit Payment</button> <br></br>
       </form>
     </div>
   )
