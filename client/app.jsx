ee440e6d5c77cc37722a8a60cff47136ba6b14b3
raphspies <raphspies@gmail.com>
2021-05-27 10:45:11 -0700

    React now renders without webpack; added CreateAccount module

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 
-var App = () => {
+class App extends React.Component {
+  constructor() {
+    super(props)
+    this.state = {
+      whichScreen: "CreateAccount"
+    }
+  }
 
+  render() {
+    if (this.state.whichScreen === "CreateAccount") {
+      return (
+        <div>
+          <CreateAcount />
+        </div>
+      )
+    }
+  }
+}
+
+var CreateAcount = () => {
+  var handleSubmit = (event) => {
+    console.log('create account clicked!')
+    event.preventDefault()
+  }
   return (
-    <div>
-      Hello from React!
+    <div id="createAccount">
+      <form>
+        Creat Account: <br></br>
+        Name: &nbsp; &nbsp; &nbsp; &nbsp;<input id="createName" placeholder="Enter name"></input> <br></br>
+        Email: &nbsp; &nbsp;&nbsp; &nbsp; <input id="createEmail" placeholder="Enter email addres"></input> <br></br>
+        Password:  <input id="createPassword" placeholder="Enter password"></input> <br></br>
+        <button id="createAccountButton" onClick={handleSubmit}>Submit</button> <br></br>
+      </form>
     </div>
   )
 }
--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public')) //path.join?
+
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('hello, world!')
+// app.get('/', (req, res) => {
+//   res.send('hello, world from Express!')
+// })
+
+app.post('/', (req, res) => {
+  res.send('POST')
 })
 
+app.put('/', (req, res) => {
+  res.send('PUT')
+})
+
+app.delete('/', (req, res) => {
+  res.send('DELETE')
+})
