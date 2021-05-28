7a55f4f75c7265d4668fe5faa783bc7235fed71a
raphspies <raphspies@gmail.com>
2021-05-27 19:13:39 -0700

    Updated shipping info form with proper event handlers (but no endpoint handling on server)

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,9 @@
-// import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      whichScreen: "CreateAccount"
+      whichScreen: "EnterShippingInfo"
     }
     this.loadNext = this.loadNext.bind(this);
   }
@@ -26,18 +25,18 @@ class App extends React.Component {
     } else if (this.state.whichScreen === "EnterShippingInfo") {
       return (
         <div>
-          <EnterShippingInfo />
+          <EnterShippingInfo loadNext={this.loadNext}/>
         </div>
       )
     } else if (this.state.whichScreen === "EnterPaymentInfo") {
       return (
         <div>
-          <EnterPaymentInfo />
+          <EnterPaymentInfo/>
         </div>
       )
     }
   }
-}
+} //end App
 
 
 //Form1 NOTE -- password must be 15 or less characters long
@@ -59,12 +58,12 @@ class CreateAccount extends React.Component {
     console.log('create account clicked!')
     event.preventDefault()
     axios.post(`/account/${this.state.name}/${this.state.email}/${this.state.password}`)
-    .catch((err) => {
-      console.log(err)
+    .catch(err => {
+      console.log(err);
     })
     .then((res)=> {
-      console.log(res)
-      this.props.loadNext("EnterShippingInfo")
+      console.log(res);
+      this.props.loadNext("EnterShippingInfo");
     });
 }
 
@@ -106,38 +105,91 @@ class CreateAccount extends React.Component {
       </div>
     )
   }
-}
+} //end Form1
 
 //Form2
-var EnterShippingInfo = () => {
-  var handleSubmit = (event) => {
+class EnterShippingInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      address1: "",
+      address2: "",
+      city: "",
+      estate: "",
+      zip: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.enterAddress1 = this.enterAddress1.bind(this);
+    this.enterAddress2 = this.enterAddress2.bind(this);
+    this.enterCity = this.enterCity.bind(this);
+    this.enterState = this.enterState.bind(this);
+    this.enterZip = this.enterZip.bind(this);
+  }
+
+  handleSubmit(event) {
     console.log('shipping info clicked!')
     event.preventDefault()
+    axios.post(`/shipping/${this.state.address1}/${this.state.address2}/${this.state.city}/${this.state.estate}/${this.state.zip}`)
+    .catch(err => {
+      console.log(err);
+    })
+    .then(res => {
+      console.log(res);
+      this.props.loadNext("EnterPaymentInfo");
+    })
   }
-  return (
-    <div id="enterShippingInfo">
-      <form>
-        <h2> Enter Shipping Information: </h2>
-        <label htmlFor="address1" className="label">Address (line 1):</label>
-        <input id="address1" className ="inputField" placeholder="Enter Address, line 1"></input> <br></br>
 
-        <label htmlFor="address2" className="label">Address (line 2): </label>
-        <input id="address2" className ="inputField" placeholder="Enter Address, line 2"></input> <br></br>
+  enterAddress1(event) {
+    this.setState({ address1: event.target.value })
+    console.log(this.state.address1)
+  }
 
-        <label htmlFor="enterCity" className="label">City: </label>
-        <input id="enterCity" className ="inputField" placeholder="Enter city"></input> <br></br>
+  enterAddress2(event) {
+    this.setState({ address2: event.target.value })
+    console.log(this.state.address2)
+  }
 
-        <label htmlFor="enterState" className="label">State: </label>
-        <input id="enterState" className ="inputField" placeholder="Enter state"></input> <br></br>
+  enterCity(event) {
+    this.setState({ city: event.target.value })
+    console.log(this.state.city)
+  }
 
-        <label htmlFor="enterZip" className="label">Zip Code: </label>
-        <input id="enterZip" className ="inputField" placeholder="Enter zip code"></input> <br></br> <br></br>
+  enterState(event) {
+    this.setState({ estate: event.target.value })
+    console.log(this.state.estate)
+  }
 
-        <button id="createAccountButton" onClick={handleSubmit}>Submit</button> <br></br>
-      </form>
-    </div>
-  )
-}
+  enterZip(event) {
+    this.setState({ zip: event.target.value })
+    console.log(this.state.zip)
+  }
+
+  render() {
+    return (
+      <div id="enterShippingInfo">
+        <form>
+          <h2> Enter Shipping Information: </h2>
+          <label htmlFor="address1" className="label">Address (line 1):</label>
+          <input id="address1" className ="inputField" placeholder="Enter Address, line 1" onChange={this.enterAddress1}></input> <br></br>
+
+          <label htmlFor="address2" className="label">Address (line 2): </label>
+          <input id="address2" className ="inputField" placeholder="Enter Address, line 2" onChange={this.enterAddress2}></input> <br></br>
+
+          <label htmlFor="enterCity" className="label">City: </label>
+          <input id="enterCity" className ="inputField" placeholder="Enter city" onChange={this.enterCity}></input> <br></br>
+
+          <label htmlFor="enterState" className="label">State: </label>
+          <input id="enterState" className ="inputField" placeholder="Enter state" onChange={this.enterState}></input> <br></br>
+
+          <label htmlFor="enterZip" className="label">Zip Code: </label>
+          <input id="enterZip" className ="inputField" placeholder="Enter zip code" onChange={this.enterZip}></input> <br></br> <br></br>
+
+          <button id="createAccountButton" onClick={this.handleSubmit}>Submit</button> <br></br>
+        </form>
+      </div>
+    )
+  }
+} //end Form2
 
 //Form3
 var EnterPaymentInfo = () => {
@@ -165,6 +217,6 @@ var EnterPaymentInfo = () => {
       </form>
     </div>
   )
-}
+} // end Form3
 
 ReactDOM.render(<App />, document.getElementById("root"))
